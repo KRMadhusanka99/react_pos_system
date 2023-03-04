9a219a919a6d3a444a7171ed3cf3d215474a22f3
KRMadhusanka99 <Kkrmadhu1999@gmail.com>
2023-03-05 00:03:50 +0530

    'addMouseEvent'

--- src/components/FoodTypeTile/FoodTypeTile.js
+++ src/components/FoodTypeTile/FoodTypeTile.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Style from './FoodTypeTile.module.css';
 import Breakfast from '../../assets/breakfast.png';
 
 const FoodTypeTile = () => {
+
+    const [mouseState, setMouseEnter] = useState(false);
+
+    const mouseEnterHandler = () =>{
+        setMouseEnter(true);
+    }
+
+    const mouseLeaveHandler = () =>{
+        setMouseEnter(false);
+    }
+
     return (  
-        <div>
-            <img src={Breakfast} alt="" />
+        <div 
+            className={Style.outer} 
+            onMouseEnter={mouseEnterHandler} 
+            onMouseLeave={mouseLeaveHandler}
+            style={{backgroundColor: !mouseState ? "#B4FFF6":"red"}}    
+        >
+            <img className={Style.img} src={Breakfast} alt="" />
+            <div className={Style.title_holder}>
+                <span className={Style.main_title}>Breakfast</span>
+                <span className={Style.sub_title}>13 Items</span>
+            </div>
         </div>
     );
 }
